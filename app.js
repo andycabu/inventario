2174bb8a13a43f31c60482b496699459cec97eda
andycabu-portatil <andycabu@gmail.com>
2023-09-08 02:19:39 +0200

    nuevos cambios en la estructura

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 import express from "express";
 import indexRoutes from "./routes/indexRoutes.js";
-import path from "path"; // Importamos el módulo path para poder usar la función join
 
 const app = express();
-app.use(express.static("src"));
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname));
 
 //Middlewares
 app.use(express.json());
