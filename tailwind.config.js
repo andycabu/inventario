3193704ee759d949f85857109add1db11043c38b
andycabu-PC <andycabu@gmail.com>
2023-09-09 22:19:26 +0200

    empezando a dar estilos tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 };
